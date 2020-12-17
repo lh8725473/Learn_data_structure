197f38630338d1f7122e197f239e3cac2593fd5f
liuh <171213574@qq.com>
2020-12-17 17:08:49 +0800

    重学数据结构之-栈(stack)的实现

---
+++ src/stack/stack-array.ts
@@ -0,0 +1,36 @@
+class StackArrayTemp<T> {
+  private items: T[];
+
+  constructor() {
+    this.items = [];
+  }
+
+  push(item: T) {
+    this.items.push(item);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  toString() {
+    return this.items.toString();
+  }
+
+}
---
+++ src/stack/stack.ts
@@ -0,0 +1,55 @@
+class Stack<T> {
+  private count: number;
+  private items: any;
+
+  constructor() {
+    this.count = 0;
+    this.items = {};
+  }
+
+  push(item: T) {
+    this.items[this.count] = item;
+    this.count++;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    this.count--;
+    const item = this.items[this.count];
+    delete this.items[this.count];
+    return item;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.count - 1];
+  }
+
+  isEmpty() {
+    return this.count === 0;
+  }
+
+  clear() {
+    this.count = 0;
+    this.items = {};
+  }
+
+  size() {
+    return this.count;
+  }
+
+  toSting() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    let objStr = `${this.items[0]}`;
+    for (let i = 1; i < this.count; i++) {
+      objStr = `${objStr},${this.items[i]}`;
+    }
+    return objStr;
+  }
+}
